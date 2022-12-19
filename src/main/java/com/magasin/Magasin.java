5cad44d119555075b9a2c54c533227e42a66b3ab
charlene.dautun <cd.webdesign.graphism@gmail.com>
2022-12-19 16:53:06 +0100

    start golden master

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,21 +9,28 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            // Si le nom de l'item est différent de "Comté" et de "Passe VIP Concert" -> donc égal à Kryptonite ou autre
             if (!items[i].name.equals("Comté")
                     && !items[i].name.equals("Pass VIP Concert")) {
+                // Si la qualité de l'item est supérieur à 0
                 if (items[i].quality > 0) {
+                    //Si le nom de l'item n'est pas Kryptonite -> donc est égal à autre
                     if (!items[i].name.equals("Kryptonite")) {
-                        items[i].quality = items[i].quality - 1;
+                        items[i].quality = items[i].quality - 1; // La qualité de l'item est égale à la qualité de l'item - 1 (perte de qualité à un jour passé)
                     }
                 }
+            // Sinon
             } else {
+                // Si la qualité de l'item est inférieur à 50
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
+                    items[i].quality = items[i].quality + 1; // La qualité de l'item est égale à la qualité de l'item + 1
+                    // Si le nom de l'item est égal à "Passe VIP Concert"
                     if (items[i].name.equals("Pass VIP Concert")) {
+                        // Si le nombre de jours restants de l'item est inférieur à 11
                         if (items[i].sellIn < 11) {
+                            // Si la qualité de l'item est inférieur à 50
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 1; // La qualité de l'item est égale à la qualité de l'item + 1
                             }
                         }
 
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -2,16 +2,80 @@ package com.magasin;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("foo", 0, 0)};
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    //TESTS PASS VIP CONCERT
+    @Test
+    // 1) SpellIn entre 50 et 10 == Quality +1
+    void testPassVIPSellInIs12QualityIs20() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 12, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality(); // Output "Pass VIP Concert, 11, 21"
+        System.out.println(app.items[0]);
+        assertEquals("Pass VIP Concert, 11, 21", app.items[0].toString());
+    }
+    @Test
+    // 2) SpellIn entre 10 et 6 == Quality +2
+    void testPassVIPSellInIs10QualityIs20() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 10, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality(); // Output "Pass VIP Concert, 9, 22"
+        System.out.println(app.items[0]);
+        assertEquals("Pass VIP Concert, 9, 22", app.items[0].toString());
+    }
+
+    @Test
+    // 3) SpellIn entre 5 et 0 == Quality +3 -> ici 5
+    void testPassVIPSellInIs5QualityIs20() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 5, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality(); // Output "Pass VIP Concert, 4, 23"
+        System.out.println(app.items[0]);
+        assertEquals("Pass VIP Concert, 4, 23", app.items[0].toString());
+    }
+
+    @Test
+    // 4) SpellIn après 0 (-1) == Quality = 0
+    void testPassVIPSellInIs0QualityIs20() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 0, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality(); // Output "Pass VIP Concert, -1, 0"
+        System.out.println(app.items[0]);
+        assertEquals("Pass VIP Concert, -1, 0", app.items[0].toString());
+    }
+
+    @Test
+    // Item se dégrade 2 fois plus vite après date de péremption
+    void testClassicItemPeremptionPast0(){
+        Item[] items = new Item[] {new Item("Patate", 0, 10)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        System.out.println(app.items[0]);
+        assertEquals("Patate, -1, 8", app.items[0].toString());
+    }
+
+    @Test
+    // Un produit ne peut pas avoir une qualité supérieur à 50
+    // (ex : le Comté qui augmente sa qualité avec le temps ne peut dépasser 50 de qualité)
+    void testClassicItemQualitySup50(){
+        Item[] items = new Item[] {new Item("Comté", 10, 48)};
+        Magasin app = new Magasin(items);
+        for (int i = 0; i < 3; i++){
+            app.updateQuality();
+            System.out.println(app.items[0]);
+        }
+        assertEquals("Comté, 7, 50", app.items[0].toString());
+    }
 }
